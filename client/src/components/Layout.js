fe86f7270deea9cf2e3b0590680d022925c7132f
Your Name <you@example.com>
2022-05-22 13:11:22 +0500

    home department complete

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -2,13 +2,16 @@ import React, { useEffect, useState } from "react";
 import Header from "./header/Header";
 import { useLocation } from "react-router-dom";
 import Footer from "./footer/Footer";
+import { SIGN_IN } from "./services/constants";
 
 const Layout = (props) => {
   const location = useLocation();
   const [header, setHeader] = useState(false);
+  const [footer, setFooter] = useState(true);
 
   useEffect(() => {
     setHeader(true);
+    if (location.pathname === SIGN_IN) setFooter(false);
   }, [location.pathname]);
 
   useEffect(() => {
@@ -19,7 +22,7 @@ const Layout = (props) => {
     <div id="layout-wrapper">
       {header && <Header />}
       <div className="body-content">{props.children}</div>
-      <Footer />
+      {footer && <Footer />}
     </div>
   );
 };
