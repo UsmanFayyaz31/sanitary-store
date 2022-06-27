1909242ea184acfeb976d279f47ef63a33a8fcbd
Your Name <you@example.com>
2022-06-27 18:49:13 +0500

    cors update

--- server/index.js
+++ server/index.js
@@ -22,6 +22,7 @@ mongoose.connection.on("connected", () => {
 });
 
 app.use(cors());
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
